9b63af86597a4f688afd25d962bedb18d78d87cd
Md. Masrafi Bin Seraj <masrafi190116@gmail.com>
2024-07-08 11:20:03 +0600

    Games API added

---
+++ Dtos/GameDto.cs
@@ -0,0 +1,3 @@
+﻿namespace GameStore.Dtos;
+
+public record class GameDto(int ID, string Name,string Genre,decimal Price, DateOnly ReleaseDate);
--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
+using GameStore.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+List<GameDto> games=[
+    new (1,"NFS","Race",500M,new DateOnly(2000,2,10)),
+    new (2,"NeoGeo","Arcade",400M,new DateOnly(1997,3,2)),
+    new (2,"GTA","Open-World",600M,new DateOnly(2003,12,25))
+];
+
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("games",() => games);
 app.Run();
 
 
