41636c26600d185e256b002661664a9516e77002
Md. Masrafi Bin Seraj <masrafi190116@gmail.com>
2024-07-10 22:32:42 +0600

    Separate file created for GamesEndpoints

---
+++ Endpoints/GamesEndpoints.cs
@@ -0,0 +1,67 @@
+using GameStore.Dtos;
+
+namespace GameStore.Endpoints;
+
+public static class GamesEndpoints
+{
+
+    const string getGameEndpoint = "GetGame";
+    private static readonly List<GameDto> games = [
+        new (1, "NFS", "Race", 500, new DateOnly(2000, 2, 10)),
+    new (2, "NeoGeo", "Arcade", 400, new DateOnly(1997, 3, 2)),
+    new (3, "GTA", "Open-World", 600, new DateOnly(2003, 12, 25))
+    ];
+
+    public static WebApplication MapGameEndpoints(this WebApplication app)
+    {
+
+        app.MapGet("/", () => "Welcome to the games store....!");
+        app.MapGet("games", () => games);
+        app.MapGet("games/{id}", (int id) =>
+        {
+            GameDto? game = games.Find(game => game.ID == id);
+
+            return game is null ? Results.NotFound() : Results.Ok(game);
+            // games.Find(game => game.ID == id);
+
+        }).WithName(getGameEndpoint);
+
+        app.MapPost("games", (CreateGameDto newGame) =>
+        {
+            GameDto game = new(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+            games.Add(game);
+
+            return Results.CreatedAtRoute(getGameEndpoint, new { id = game.ID }, game);
+        });
+
+        app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+        {
+            var index = games.FindIndex(game => game.ID == id); // confusion with the game
+            if (index == -1) return Results.NotFound();
+            games[index] = new(
+                id,
+                updatedGame.Name,
+                updatedGame.Genre,
+                updatedGame.Price,
+                updatedGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        app.MapDelete("games/{id}", (int id) =>
+        {
+            games.RemoveAll(game => game.ID == id);
+            return Results.NoContent();
+        }
+        );
+
+        return app;
+    }
+}
--- obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55fbc2e002487d02ea0ef1d743d0913824ff7f8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5efafa800f905a6e088bd244b0f8b42903e6ca75")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
