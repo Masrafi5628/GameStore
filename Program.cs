eb7d3606f462294f564ebba863ac306a63647c1c
Md. Masrafi Bin Seraj <masrafi190116@gmail.com>
2024-07-12 03:08:24 +0600

    Migrations Folder Added

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 using System.Reflection.Metadata.Ecma335;
 using GameStore.Dtos;
 using GameStore.Endpoints;
+using GameStore.Data;
 
 // use f5 for debug
 var builder = WebApplication.CreateBuilder(args);
-
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
 var app = builder.Build();
 app.MapGet("/", () => "Welcome to the games store....!");
 
 app.MapGameEndpoints();
+
 
 app.Run();
 
